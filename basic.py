2ecaddc770fc82b595b9971f4722f94f14d1dd9c
dattatraygavali.rts <dattatraygavali.rts@gmail.com>
2020-05-02 19:47:23 +0530

    python basice

---
+++ basic.py
@@ -0,0 +1,18 @@
+print(type(2+4))
+print(type(2-4))
+print(type(2*4))
+print(type(2/4))
+
+print(bin(5))
+
+# Varible
+# 1. ex - some_vlue
+# 2. Starting with lowercase or underscore
+# 3. latter, number, underscore
+# 4. case sensitive
+# 5. Don't overwrite varible
+
+# augmented assigned operator
+some_value = 5
+some_value += 2
+print(some_value)
