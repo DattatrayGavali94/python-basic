61552b8c5b952b6c0232fefc852fd83c651e9482
dattatraygavali.rts <dattatraygavali.rts@gmail.com>
2020-05-06 00:16:46 +0530

    list

---
+++ list.py
@@ -0,0 +1,38 @@
+li = ['a','b','c','d','e','f','g','x','d']
+# dding
+# li.extend([100,1001])
+
+li.sort()
+li.reverse()
+print(li)
+# list slicing
+print(li[::-1])
+
+
+# li.pop(5)
+# li.insert(2,'D')
+# # removing
+# li.remove(100)
+# new_list = li.copy()
+# print(li)
+# print(new_list)
+# # removing
+# li.clear()
+
+
+# print(li.index('d',0,4))
+# print('d' in li)
+# print(li.count('d'))
+
+# Note : sorted produces new arry
+
+# list count
+print(len(li))
+
+# range
+print(list(range(100)))
+
+# join
+
+new_sentence = ' '.join(['hi','dattatray'])
+print(new_sentence)
